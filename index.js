5a54fba87faeb5914a23e8c438247873cd3436d8
Harold <haroldbarron13@gmail.com>
2022-12-15 13:39:38 -0600

    Creating app and lisening the defined port

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import express from 'express'
+
+const app = express()
+const port = 3000 || process.env.port
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}` )
+})
 console.log("index js")
