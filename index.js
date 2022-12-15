e1a9e194fcfe11d13b9a97c791ba50d54af97e28
Harold <haroldbarron13@gmail.com>
2022-12-15 13:40:12 -0600

    Routing main page

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import express from 'express'
 const app = express()
 const port = 3000 || process.env.port
 
+
+app.get('/',(req,res)  =>{
+    res.json({msg:'Concectado desde Express'})
+})
+
+
+
+
+
 app.listen(port, () =>{
     console.log(`Server running on port ${port}` )
 })
