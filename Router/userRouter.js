2f7dd95dfd34645d4c4ebb61ee7b726b4b7f2256
Harold <haroldbarron13@gmail.com>
2022-12-15 17:56:45 -0600

    Adding sign up router

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {loginForm} from '../controllers/userController.js'
+import {loginForm, signUpForm} from '../controllers/userController.js'
 const router = express.Router()
 
 router.get('/login',loginForm)
+router.get('/sign-up',signUpForm)
 
 export default router
