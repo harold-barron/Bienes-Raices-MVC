117475fa058189703dee54720daf4da611e2e2b4
Harold <haroldbarron13@gmail.com>
2023-01-04 15:55:37 -0600

    Creating endpoint of reset password

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
-import {loginForm, signUpForm} from '../controllers/userController.js'
+import {loginForm, signUpForm,resetPasswordForm} from '../controllers/userController.js'
 const router = express.Router()
 
 router.get('/login',loginForm)
 router.get('/sign-up',signUpForm)
-
+router.get('/reset-password', resetPasswordForm)
 export default router
