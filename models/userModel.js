6d86b3cd9b6ad5008f5775b9fba93a98d95f43f1
Harold <haroldbarron13@gmail.com>
2023-01-10 15:12:15 -0600

    Adding email,password,token and confirmed atributes

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,16 @@ const User = db.define('users', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
-    }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: DataTypes.STRING,
+    confirmed: DataTypes.BOOLEAN
 
 })
