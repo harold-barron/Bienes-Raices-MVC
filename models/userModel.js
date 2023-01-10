73554276b604774cf9cc2ab25cb089c40099283c
Harold <haroldbarron13@gmail.com>
2023-01-10 15:03:18 -0600

    adding name atribute

--- models/userModel.js
+++ models/userModel.js
@@ -2,5 +2,9 @@ import { DataTypes } from "sequelize";
 import db from "../db/db.js";
 
 const User = db.define('users', {
-    
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
 })
