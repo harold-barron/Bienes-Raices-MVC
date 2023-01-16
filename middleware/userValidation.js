d4ed4fef23a2a218652d9da8b9fbf5a8a6326db9
Harold <haroldbarron13@gmail.com>
2023-01-16 17:15:33 -0600

    Creating middleware to validate de information sended by the client to the server

---
+++ middleware/userValidation.js
@@ -0,0 +1,49 @@
+import { check, validationResult } from "express-validator";
+const userModelValidation = async (req,res,next) =>{
+    try{
+        await check('name').notEmpty().withMessage('Please write your name').run(req)
+        await check('email').notEmpty().withMessage('Please write a valid email direcction').run(req)
+        await check('password').notEmpty().withMessage('Please write your password').isLength({min:6}).withMessage('Your password must have at least 6 digits').run(req)
+        await check('repeatedPassword').equals(req.body.password).withMessage('Passwords do not match').run(req)
+        let nameResult = validationResult(req)
+        if(!nameResult.isEmpty()){
+            let resultsArray = nameResult.array()
+            let  errors = {}
+            const errorsArray = resultsArray.forEach(element => {                    
+                if(element.param === 'name'){
+                    errors.name = element.msg
+                }  
+                if(element.param === 'email'){
+                    errors.email = element.msg
+                }   
+                if(element.param === 'password'){
+                    errors.password = element.msg
+                    
+                }    
+                if(element.param === 'repeatedPassword'){
+                    errors.repeatedPassword = element.msg
+                    
+                    console.log(req.body.repeatedPassword)
+                } 
+            });
+            console.log(errors)
+            return res.render('auth/sign_up', {
+                page: 'Create account',
+                nameError:errors.name,
+                emailError:errors.email,
+                passwordError:errors.password,
+                repeatedPasswordError:errors.repeatedPassword
+            })
+            
+        }
+        
+        next()
+    }catch(error){
+        console.log(error)
+        return res.json(error)
+    }
+}
+
+export{
+    userModelValidation
+} 
