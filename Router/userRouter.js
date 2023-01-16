518f1c63ad2049825bb57a1f180deee35b96b066
Harold <haroldbarron13@gmail.com>
2023-01-16 17:11:19 -0600

    Adding middleware funtion

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {loginForm, signUpForm,createAccount,resetPasswordForm} from '../controllers/userController.js'
+import {userModelValidation} from "../middleware/userValidation.js"
 const router = express.Router()
 
 router.get('/login',loginForm)
 router.get('/sign-up',signUpForm)
-router.post('/sign-up',createAccount)
+router.post('/sign-up',userModelValidation,createAccount)
 
 router.get('/reset-password', resetPasswordForm)
 
