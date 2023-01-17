d0aa8693b8964b4ae49c41a1951706a276298034
Harold <haroldbarron13@gmail.com>
2023-01-17 17:14:14 -0600

    Sending confirmation email

---
+++ utils/emails.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer'
+
+const confirmationEmail = async ({name,email,token}) =>{
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
+
+    await transport.sendMail({
+        from: 'BienesRaices.com',
+        to: email,
+        subject: 'Please confirm your email',
+        text: 'Pleas confirm your account in the link',
+        html: `
+            <p> Hi ${name}, please validate your account in bienesRaices.com</p>
+            <a href="${process.env.SERVER_URL}:${process.env.PORT}/auth/confirm/${token}">Click here to confirm</a>
+        `
+    })
+}
+
+export {
+    confirmationEmail
+}
