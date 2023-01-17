68c43309b5f96f21f85002258ccafec60fe5bb43
Harold <haroldbarron13@gmail.com>
2023-01-17 17:17:34 -0600

    Adding router to validate token

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {loginForm, signUpForm,createAccount,resetPasswordForm} from '../controllers/userController.js'
+import {loginForm, signUpForm,createAccount,validateAccount,resetPasswordForm} from '../controllers/userController.js'
 import {userModelValidation,emailValidation} from "../middleware/userValidation.js"
 const router = express.Router()
 
 router.get('/login',loginForm)
 router.get('/sign-up',signUpForm)
 router.post('/sign-up',userModelValidation,emailValidation,createAccount)
+router.get('/confirm/:token', validateAccount)
 
 router.get('/reset-password', resetPasswordForm)
 
