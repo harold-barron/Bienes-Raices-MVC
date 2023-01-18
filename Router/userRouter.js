cd39ad618a530212e811eea35369e35ebf81920f
Harold <haroldbarron13@gmail.com>
2023-01-18 14:01:27 -0600

    Adding get method to validate reset pass token

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {loginForm, signUpForm,createAccount,validateAccount,resetPasswordForm,resetPassword} from '../controllers/userController.js'
+import {loginForm, signUpForm,createAccount,validateAccount,resetPasswordForm,resetPassword,validateResetToken} from '../controllers/userController.js'
 import {userModelValidation,emailValidation,resetPasswordValidation} from "../middleware/userValidation.js"
 const router = express.Router()
 
@@ -10,5 +10,5 @@ router.get('/confirm/:token', validateAccount)
 
 router.get('/reset-password', resetPasswordForm)
 router.post('/reset-password',resetPasswordValidation, resetPassword)
-
+router.get('/newPassword/:token',validateResetToken)
 export default router
