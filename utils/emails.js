fd4e1c5853a3f787f4201b330699abd256579e8d
Harold <haroldbarron13@gmail.com>
2023-01-18 13:50:25 -0600

    Creating resetPAsswordEmail function to send email with instructions

--- utils/emails.js
+++ utils/emails.js
@@ -13,15 +13,37 @@ const confirmationEmail = async ({name,email,token}) =>{
     await transport.sendMail({
         from: 'BienesRaices.com',
         to: email,
-        subject: 'Please confirm your email',
-        text: 'Pleas confirm your account in the link',
+        subject: 'Restore yout passwsord',
+        text: 'Please confirm your account in the link',
         html: `
-            <p> Hi ${name}, please validate your account in bienesRaices.com</p>
+            <p> Hi ${name}, click in  the click below to reset the password of your account in bienesRaices.com</p>
             <a href="${process.env.SERVER_URL}:${process.env.PORT}/auth/confirm/${token}">Click here to confirm</a>
         `
     })
 }
 
+const resetPasswordEmail = async ({name,email,token}) =>{
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
+    await transport.sendMail({
+        from: 'BienesRaices.com',
+        to: email,
+        subject: 'Please confirm your email',
+        text: 'Pleas confirm your account in the link',
+        html: `
+            <p> Hi ${name}, please validate your account in bienesRaices.com</p>
+            <a href="${process.env.SERVER_URL}:${process.env.PORT}/auth/newPassword/${token}">Click here to confirm</a>
+        `
+    })
+    
+}
 export {
-    confirmationEmail
+    confirmationEmail,
+    resetPasswordEmail
 }
