eed68b682c7d32a455c94119fbd22662b756d47c
Harold <haroldbarron13@gmail.com>
2023-01-25 17:40:40 -0600

    Creating create createJWT function to generate a jwt with the id of the user

--- utils/tokens.js
+++ utils/tokens.js
@@ -1,7 +1,12 @@
 
+import { data } from 'autoprefixer';
+import { jwt } from 'jsonwebtoken'
+
 const generateID =  () => Date.now().toString(32)+ Math.random().toString(32).substring(2);
 
-
+const createJWT= (id) =>{jwt.sign({id}, process.env.JWT_SECRET,{expiresIn:'1d'})
+}
 export {
-    generateID
+    generateID,
+    createJWT
 }
